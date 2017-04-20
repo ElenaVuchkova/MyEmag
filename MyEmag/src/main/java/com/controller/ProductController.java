9ac85b1788c018a700a94dd27613111f55d0a643
ElenaVuchkova <lenetuuu@gmail.com>
2017-04-20 17:39:12 +0300

    import libs

--- MyEmag/src/main/java/com/controller/ProductController.java
+++ MyEmag/src/main/java/com/controller/ProductController.java
@@ -8,6 +8,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
 import com.model.Product;
 import com.model.dao.ProductDAO;
 
